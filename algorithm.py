ea61346b6d4eafdf43bce21e74e1e5a2931be756
Konstantin <75016737+konstantinkonstantinovich@users.noreply.github.com>
2022-05-04 22:16:42 +0300

    rewritten the whole algorithm

--- algorithm.py
+++ algorithm.py
@@ -4,9 +4,6 @@ from math import *
 
 class GeneticAlgorithm:
 
-    def __parse_fitness_func(self, func):
-        return sympify(func)
-
     def __init__(self, num_iterations, num_chromosomes, num_genes, mutation, crossing_over, selection_f):
         self.number_of_iterations = int(num_iterations)
         self.number_of_chromosomes = int(num_chromosomes)
@@ -14,9 +11,13 @@ class GeneticAlgorithm:
         self.mutation_probability = float(mutation)
         self.crossing_over_probability = float(crossing_over)
         self.selection_factor = float(selection_f)
+        self.max_fitness = []
+        self.avg_fitness = []
+        self.population = []
+
 
     def population_initialization(self):
-        return [[random.randrange(0, 2) for _ in range(self.number_of_genes) ] for _ in range(self.number_of_chromosomes)]
+        self.population = [[random.randint(0, 1) for _ in range(self.number_of_genes) ] for _ in range(self.number_of_chromosomes)]
 
     def __get_bin_value(self, chromosome, position, size):
         val = 0
@@ -26,51 +27,47 @@ class GeneticAlgorithm:
 
     def __get_fitness(self, chromosome):
         x, y = symbols('x y')
-        x_value = self.__get_bin_value(chromosome, 0, len(chromosome)//2)*100.0-50.0
-        y_value = self.__get_bin_value(chromosome, len(chromosome)//2, len(chromosome)//2)*100.0-50.0
-        f = self.__parse_fitness_func("x**2/200 + y**2/200 + cos(x)*cos(y)")
-        return -f.subs([(x, x_value), (y, y_value)])
+        x_value = self.__get_bin_value(chromosome, 0, self.number_of_genes)*100.0-50.0
+        # y_value = self.__get_bin_value(chromosome, self.number_of_genes//2, self.number_of_genes//2)*100.0-50.0
+        f = simplify("x**2 + 4", transformations='all')
+        return -f.subs([(x, x_value)])
 
-    def selection(self, population):
+    def selection(self):
         fitness_table = []
         new_population = []
         n = 0
-        for chromosome in population:
+        for chromosome in self.population:
             fitness_table.append(self.__get_fitness(chromosome))
         min_fitness = min(fitness_table)
         max_fitness = max(fitness_table)
+        self.max_fitness.append(max_fitness)
+        self.avg_fitness.append(sum(fitness_table)/self.number_of_chromosomes)
+        self.population.sort(key=self.__get_fitness)
         if max_fitness!=min_fitness:
             while n < self.number_of_chromosomes:
                 num = random.randint(0, self.number_of_chromosomes-1)
-                if self.selection_factor*random.random() < (fitness_table[num]-min_fitness)/(max_fitness-min_fitness):
-                    new_population.append(population[num])
+                if (self.selection_factor*random.random()) < ((fitness_table[num]-min_fitness)/(max_fitness-min_fitness)):
+                    new_population.append(self.population[num])
                     n+=1
+            self.population = new_population
 
-        return new_population
-
-    def mutation(self, population):
-        for chromosome in range(self.number_of_chromosomes):
-            for gene in range(self.number_of_genes):
-                if random.random() < self.mutation_probability:
-                    population[chromosome][gene] = 1 - population[chromosome][gene]
-        return population
-
-    def crossing_over(self, population):
+    def mutation(self):
         for i in range(self.number_of_chromosomes):
+            for j in range(self.number_of_genes):
+                if random.random() < self.mutation_probability:
+                    self.population[i][j] = 1 - self.population[i][j]
+
+    def crossing_over(self):
+        for i in range(0, self.number_of_chromosomes, 2):
             if random.random() < self.crossing_over_probability:
-                j = random.randint(0, self.number_of_chromosomes-1)
-                if (i!=j):
-                    pos = random.randint(1, self.number_of_genes)
-                    for k in range(pos):
-                        temp = population[i][k]
-                        population[i][k] = population[j][k]
-                        population[j][k] = temp
-        return population
+                pos = random.randint(1, self.number_of_genes-1)
+                j = i+1
+                for k in range(pos):
+                    temp = self.population[i][k]
+                    self.population[i][k] = self.population[j][k]
+                    self.population[j][k] = temp
 
     def epoch(self):
-        for _ in range(self.number_of_iterations):
-            population = self.population_initialization()
-            mutation = self.mutation(population)
-            crossing_over = self.crossing_over(mutation)
-            result = self.selection(crossing_over)
-            print(result)
+        self.mutation()
+        self.crossing_over()
+        self.selection()
--- app.py
+++ app.py
@@ -15,7 +15,11 @@ def main():
         selection_f = request.form.get('selection_f')
 
         genetic_alg = GeneticAlgorithm(number_of_iterations, number_of_chromosomes, number_of_genes, mutation_probability, crossing_over_probability, selection_f)
-        result = genetic_alg.epoch()
-        print(result)
+        genetic_alg.population_initialization()
+        for i in range(genetic_alg.number_of_iterations):
+            print(i)
+            genetic_alg.epoch()
+
+        result = [list(range(genetic_alg.number_of_iterations)), genetic_alg.max_fitness, genetic_alg.avg_fitness]
 
     return render_template('main.html', result=result)
